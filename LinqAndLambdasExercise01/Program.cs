91dd0539a248dcca4b88ff84c00678d40bc05c19
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-25 15:16:44 +0200

    Add: linq

--- LinqAndLambdasExercise01/Program.cs
+++ LinqAndLambdasExercise01/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace LinqAndLambdasExercise01
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            IEnumerable<Comic> mostExpensive =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby Comic.Prices[comic.Issue] descending
+                select comic;
+
+            foreach (Comic comic in mostExpensive)
+                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
         }
     }
 }
